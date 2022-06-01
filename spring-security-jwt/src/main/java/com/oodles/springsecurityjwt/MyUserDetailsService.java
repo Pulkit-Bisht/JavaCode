d7af9225f1d1a0c59b5acc39dac599b39c3e8058
Pushkar Bisht <pushkar@pushkar.bisht>
2022-06-01 15:43:50 +0530

    Token Related Task Done

---
+++ spring-security-jwt/src/main/java/com/oodles/springsecurityjwt/MyUserDetailsService.java
@@ -0,0 +1,16 @@
+package com.oodles.springsecurityjwt;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("pushkar","1234",new ArrayList<>());
+    }
+}
