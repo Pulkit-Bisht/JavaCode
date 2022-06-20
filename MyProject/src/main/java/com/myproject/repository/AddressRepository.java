d9820ded4b3b941e4a4b9b4da797fd80cb5ce8d5
Pulkit-Bisht <106665379+Pulkit-Bisht@users.noreply.github.com>
2022-06-20 16:53:49 +0530

    Delete MyProject directory

--- MyProject/src/main/java/com/myproject/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.repository;
-
-import com.myproject.entities.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Integer> {
-}
--- MyProject/src/main/java/com/myproject/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.repository;
-
-import com.myproject.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person,Integer> {
-}
